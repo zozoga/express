2046e0baec16cd3cb48f735408dcc2e7b4d481b6
zozoga <noz.0813.1985@gmeil.com>
2024-06-01 10:41:51 +0900

    Add monfodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,30 +1,22 @@
 
 var express = require('express');
 var router = express.Router();
-// レスポンスのデータ（ノート全件）
-const responseObjectDataAll = {
-	textObject1: {
-		id: 1,
-		title: 'ノート１のタイトルです',
-		subTitle: 'ノート１のサブタイトルです',
-		bodyText: 'ノート１の本文です'
-	},
-	textObject2: {
-		id: 2,
-		title: 'ノート２のタイトルです',
-		subTitle: 'ノート２のサブタイトルです',
-		bodyText: 'ノート２の本文です'
-	},
-};
-/**
-* メモを全件取得するAPI
-* @returns {Object[]} data
-* @returns {number} data.id - ID
-* @returns {string} data.title - タイトル
-* @returns {string} data.text - 内容
-*/
-router.get('/', function (req, res, next) {
-	// 全件取得して返す
-	res.json(responseObjectDataAll);
+
+//接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://noz08131985:P3IeaK6Jv1QS0RkI@cluster0.uikarur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+	//データベース、コレクションを指定
+	const database = client.db('notes');
+	const notes = database.collection('notes');
+
+	//idが１のドキュメントを取得
+	const query = { id: 2 };
+	const note = await notes.findOne(query);
+
+	res.json(note);
 })
+
 module.exports = router;
